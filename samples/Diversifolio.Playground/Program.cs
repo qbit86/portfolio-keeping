fb7a190c2ca3d8b14a68330fe146230e6cbf96ca
Viktor T <qbit86@gmail.com>
2021-08-03 01:38:28 +0300

    Implement retrieving portfolio

--- samples/Diversifolio.Playground/Program.cs
+++ samples/Diversifolio.Playground/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Tinkoff.Trading.OpenApi.Models;
+using Tinkoff.Trading.OpenApi.Network;
 
 [assembly: CLSCompliant(true)]
 
@@ -10,9 +12,15 @@ namespace Diversifolio
     {
         private static async Task Main()
         {
+            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk
+
             using var reader = new StreamReader("token-tinkoff-sandbox.txt");
             string? token = await reader.ReadLineAsync().ConfigureAwait(false);
             Console.WriteLine($"{nameof(token)}: {token}");
+            using SandboxConnection connection = ConnectionFactory.GetSandboxConnection(token);
+            SandboxContext context = connection.Context;
+            Portfolio portfolio = await context.PortfolioAsync().ConfigureAwait(false);
+            Console.WriteLine(portfolio);
         }
     }
 }
