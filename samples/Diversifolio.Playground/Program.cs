dcacb9354785247d62cda4b1a4e438ea94e78e1f
Viktor T <qbit86@gmail.com>
2021-08-03 01:44:35 +0300

    Add registering account

--- samples/Diversifolio.Playground/Program.cs
+++ samples/Diversifolio.Playground/Program.cs
@@ -12,13 +12,17 @@ namespace Diversifolio
     {
         private static async Task Main()
         {
-            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk
-
             using var reader = new StreamReader("token-tinkoff-sandbox.txt");
             string? token = await reader.ReadLineAsync().ConfigureAwait(false);
             Console.WriteLine($"{nameof(token)}: {token}");
+
+            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk
             using SandboxConnection connection = ConnectionFactory.GetSandboxConnection(token);
             SandboxContext context = connection.Context;
+
+            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk/issues/25
+            SandboxAccount _ = await context.RegisterAsync(BrokerAccountType.Tinkoff).ConfigureAwait(false);
+
             Portfolio portfolio = await context.PortfolioAsync().ConfigureAwait(false);
             Console.WriteLine(portfolio);
         }
