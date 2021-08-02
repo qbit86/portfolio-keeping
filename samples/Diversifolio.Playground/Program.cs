db0421f2498bc912189c9a4f8d0a823e98f196d8
Viktor T <qbit86@gmail.com>
2021-08-03 01:33:04 +0300

    Add reading the token

--- samples/Diversifolio.Playground/Program.cs
+++ samples/Diversifolio.Playground/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 [assembly: CLSCompliant(true)]
 
@@ -6,6 +8,11 @@ namespace Diversifolio
 {
     internal static class Program
     {
-        private static void Main() { }
+        private static async Task Main()
+        {
+            using var reader = new StreamReader("token-tinkoff-sandbox.txt");
+            string? token = await reader.ReadLineAsync().ConfigureAwait(false);
+            Console.WriteLine($"{nameof(token)}: {token}");
+        }
     }
 }
