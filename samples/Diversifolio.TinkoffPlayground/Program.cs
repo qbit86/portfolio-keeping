7b98321984967b0ee0b2b5cf3aaef804651d4c30
Viktor T <qbit86@gmail.com>
2021-08-04 03:15:04 +0300

    Add RunActualCheck()

--- samples/Diversifolio.TinkoffPlayground/Program.cs
+++ samples/Diversifolio.TinkoffPlayground/Program.cs
@@ -13,8 +13,9 @@ namespace Diversifolio
     {
         private static CultureInfo F => CultureInfo.InvariantCulture;
 
-        private static async Task Main() => await RunSandboxCheck().ConfigureAwait(false);
+        private static async Task Main() => await RunActualCheck().ConfigureAwait(false);
 
+        // ReSharper disable once UnusedMember.Local
         private static async Task RunSandboxCheck()
         {
             using var reader = new StreamReader("token-tinkoff-sandbox.txt");
@@ -50,6 +51,32 @@ namespace Diversifolio
             {
                 if (instrument.Currency == Currency.Eur)
                     Console.WriteLine($"    - {instrument}");
+            }
+        }
+
+        private static async Task RunActualCheck()
+        {
+            using var reader = new StreamReader("token-tinkoff.txt");
+            string? token = await reader.ReadLineAsync().ConfigureAwait(false);
+            Console.WriteLine($"{nameof(token)}: {token}");
+
+            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk
+            using Connection connection = ConnectionFactory.GetConnection(token);
+            Context context = connection.Context;
+
+            // https://github.com/TinkoffCreditSystems/invest-openapi-csharp-sdk/blob/master/samples/TradingBot/SandboxBot.cs
+            PortfolioCurrencies portfolioCurrencies = await context.PortfolioCurrenciesAsync().ConfigureAwait(false);
+            Console.WriteLine("PortfolioCurrencies:");
+            foreach (PortfolioCurrencies.PortfolioCurrency currency in portfolioCurrencies.Currencies)
+                Console.WriteLine($"    - {currency.Currency}, {currency.Balance.ToString(F)}");
+
+            Portfolio portfolio = await context.PortfolioAsync().ConfigureAwait(false);
+            Console.WriteLine("Portfolio:");
+            foreach (Portfolio.Position position in portfolio.Positions)
+            {
+                MoneyAmount price = position.AveragePositionPrice;
+                Console.WriteLine(
+                    $"    - {position.Ticker}, {position.Balance.ToString(F)}, {price.Value.ToString(F)} {price.Currency}");
             }
         }
     }
