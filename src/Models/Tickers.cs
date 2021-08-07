d1131d30b208dd2873fa71215b494ad41682526f
Viktor T <qbit86@gmail.com>
2021-08-07 20:33:27 +0300

    Add Tickers constants

---
+++ src/Models/Tickers.cs
@@ -0,0 +1,47 @@
+namespace Diversifolio
+{
+    internal static class Tickers
+    {
+        internal const string Dsky = "DSKY";
+        internal const string Fxcn = "FXCN";
+        internal const string Fxde = "FXDE";
+        internal const string Fxdm = "FXDM";
+        internal const string Fxes = "FXES";
+        internal const string Fxgd = "FXGD";
+        internal const string Fxim = "FXIM";
+        internal const string Fxip = "FXIP";
+        internal const string Fxit = "FXIT";
+        internal const string Fxmm = "FXMM";
+        internal const string Fxrb = "FXRB";
+        internal const string Fxrl = "FXRL";
+        internal const string Fxru = "FXRU";
+        internal const string Fxrw = "FXRW";
+        internal const string Fxtb = "FXTB";
+        internal const string Fxtp = "FXTP";
+        internal const string Fxus = "FXUS";
+        internal const string Fxwo = "FXWO";
+        internal const string Mgnt = "MGNT";
+        internal const string Mvid = "MVID";
+        internal const string Rtkm = "RTKM";
+        internal const string Sbcb = "SBCB";
+        internal const string Sbgb = "SBGB";
+        internal const string Sbmx = "SBMX";
+        internal const string Sbrb = "SBRB";
+        internal const string Sbsp = "SBSP";
+        internal const string Su25083 = "SU25083RMFS5";
+        internal const string Su26214 = "SU26214RMFS5";
+        internal const string Su29012 = "SU29012RMFS0";
+        internal const string Tbio = "TBIO";
+        internal const string Tech = "TECH";
+        internal const string Tmos = "TMOS";
+        internal const string Tspx = "TSPX";
+        internal const string Vtba = "VTBA";
+        internal const string Vtbb = "VTBB";
+        internal const string Vtbe = "VTBE";
+        internal const string Vtbh = "VTBH";
+        internal const string Vtbu = "VTBU";
+        internal const string Vtbx = "VTBX";
+        internal const string Vtby = "VTBY";
+        internal const string Yndx = "YNDX";
+    }
+}
