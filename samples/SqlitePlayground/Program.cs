3f4251d1eb59267fc393955d9fd6b802b682cdd4
Viktor T <qbit86@gmail.com>
2021-08-07 14:52:15 +0300

    Add creating empty database

--- samples/SqlitePlayground/Program.cs
+++ samples/SqlitePlayground/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Data.Sqlite;
 
 [assembly: CLSCompliant(true)]
 
@@ -6,6 +9,26 @@ namespace Diversifolio
 {
     internal static class Program
     {
-        private static void Main() { }
+        private static CultureInfo F => CultureInfo.InvariantCulture;
+
+        private static void Main()
+        {
+            string dateString = DateTime.Now.ToString("yyyy-MM-dd", F);
+            string dirName = Path.Join(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SqlitePlayground");
+            Directory.CreateDirectory(dirName);
+            Console.WriteLine($"{nameof(dirName)}: {dirName}");
+            string baseName = $"SqlitePlayground-{dateString}.db";
+            string path = Path.Join(dirName, baseName);
+
+            SqliteConnectionStringBuilder connectionStringBuilder = new()
+            {
+                DataSource = path,
+                Mode = SqliteOpenMode.ReadWriteCreate
+            };
+            string connectionString = connectionStringBuilder.ToString();
+            using SqliteConnection connection = new(connectionString);
+            connection.Open();
+        }
     }
 }
