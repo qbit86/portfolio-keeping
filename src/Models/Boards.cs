6779cdc216ed64c7bfa98a7c2e07ad41828f3018
Viktor T <qbit86@gmail.com>
2021-08-07 20:37:45 +0300

    Add Boards constants

---
+++ src/Models/Boards.cs
@@ -0,0 +1,10 @@
+namespace Diversifolio
+{
+    internal static class Boards
+    {
+        internal const string Tqbr = "TQBR";
+        internal const string Tqob = "TQOB";
+        internal const string Tqtd = "TQTD";
+        internal const string Tqtf = "TQTF";
+    }
+}
