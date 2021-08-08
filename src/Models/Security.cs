c830d78672e9e605d946c9cd7c40b3097c128ad1
Viktor T <qbit86@gmail.com>
2021-08-08 16:54:40 +0300

    Add type Security

---
+++ src/Models/Security.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Diversifolio
+{
+    public sealed class Security
+    {
+        public Security(string secId, string boardId, string currencyId, int lotSize, decimal faceValue, int decimals,
+            decimal prevAdmittedQuote)
+        {
+            SecId = secId ?? throw new ArgumentNullException(nameof(secId));
+            BoardId = boardId ?? throw new ArgumentNullException(nameof(boardId));
+            CurrencyId = currencyId ?? throw new ArgumentNullException(nameof(currencyId));
+            LotSize = lotSize;
+            FaceValue = faceValue;
+            Decimals = decimals;
+            PrevAdmittedQuote = prevAdmittedQuote;
+        }
+
+        public string SecId { get; }
+        public string BoardId { get; }
+        public string CurrencyId { get; }
+        public int LotSize { get; }
+        public decimal FaceValue { get; }
+        public int Decimals { get; }
+        public decimal PrevAdmittedQuote { get; }
+    }
+}
