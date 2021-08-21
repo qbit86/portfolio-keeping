6f90250a0fa58eb23a775e4518bb5f6f3ff105b5
Viktor T <qbit86@gmail.com>
2021-08-21 17:34:58 +0300

    Add printing the positions

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 
@@ -9,16 +11,33 @@ namespace Diversifolio
 {
     internal static class Program
     {
-        // ReSharper disable once UnusedMember.Local
         private static TextWriter Out => Console.Out;
 
         private static async Task Main()
         {
-            string dirName = Path.Join(
+            string directoryPath = Path.Join(
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(Diversifolio));
             const string portfolioName = "Tinkoff";
-            await using SqliteConnection _ = await DataHelpers.CreatePortfolioConnection(dirName, portfolioName)
-                .ConfigureAwait(false);
+            await using SqliteConnection connection = await DataHelpers.CreatePortfolioConnection(
+                directoryPath, portfolioName).ConfigureAwait(false);
+
+            ImmutableDictionary<string, Position>.Builder builder =
+                ImmutableDictionary.CreateBuilder<string, Position>(StringComparer.Ordinal);
+            const string commandText = "SELECT Ticker, Balance FROM Position";
+            await using SqliteCommand command = new(commandText, connection);
+            SqliteDataReader query = command.ExecuteReader();
+            while (query.Read())
+            {
+                string ticker = query.GetString(0);
+                decimal balance = query.GetInt32(1);
+                Position position = new(ticker, balance);
+                builder[ticker] = position;
+            }
+
+            ImmutableDictionary<string, Position> positionByTicker = builder.ToImmutable();
+            var positions = positionByTicker.Values.OrderBy(it => it.Ticker, StringComparer.Ordinal).ToImmutableArray();
+            foreach (Position position in positions)
+                await Out.WriteLineAsync(position.ToString()).ConfigureAwait(false);
         }
     }
 }
