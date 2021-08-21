8f6da881e977833ac77c883f428a48b1674cc679
Viktor T <qbit86@gmail.com>
2021-08-21 15:11:49 +0300

    Add creating the portfolio database

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 [assembly: CLSCompliant(true)]
 
@@ -8,16 +10,73 @@ namespace Diversifolio
 {
     internal static class Program
     {
+        // ReSharper disable once UnusedMember.Local
         private static TextWriter Out => Console.Out;
 
         private static async Task Main()
         {
             string dirName = Path.Join(
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(Diversifolio));
-            Directory.CreateDirectory(dirName);
-            const string baseName = "Tinkoff.sql";
-            string path = Path.Join(dirName, baseName);
-            await Out.WriteLineAsync($"{nameof(path)}: {path}").ConfigureAwait(false);
+            const string portfolioName = "Tinkoff";
+            await using SqliteConnection _ = await CreatePortfolioConnection(dirName, portfolioName)
+                .ConfigureAwait(false);
+        }
+
+        private static async Task<SqliteConnection> CreatePortfolioConnection(
+            string directoryPath, string portfolioName)
+        {
+            Directory.CreateDirectory(directoryPath);
+            string databasePath = Path.Join(directoryPath, portfolioName + ".db");
+            if (!File.Exists(databasePath))
+                await CreatePortfolioDatabase(directoryPath, portfolioName, databasePath).ConfigureAwait(false);
+
+            SqliteConnectionStringBuilder connectionStringBuilder = new()
+            {
+                DataSource = databasePath,
+                Mode = SqliteOpenMode.ReadOnly
+            };
+            string connectionString = connectionStringBuilder.ToString();
+            await using SqliteConnection connection = new(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        private static async Task CreatePortfolioDatabase(
+            string directoryPath, string portfolioName, string databasePath)
+        {
+            SqliteConnectionStringBuilder connectionStringBuilder = new()
+            {
+                DataSource = databasePath,
+                Mode = SqliteOpenMode.ReadWriteCreate
+            };
+            string connectionString = connectionStringBuilder.ToString();
+            await using SqliteConnection connection = new(connectionString);
+            connection.Open();
+            await CreatePositionTable(connection).ConfigureAwait(false);
+            await PopulatePositionTable(connection, directoryPath, portfolioName).ConfigureAwait(false);
+        }
+
+        private static async Task CreatePositionTable(SqliteConnection connection)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            await using Stream stream = assembly.GetManifestResourceStream(typeof(Program), "CreatePosition.sql")!;
+            using StreamReader reader = new(stream);
+            await ExecuteReader(connection, reader).ConfigureAwait(false);
+        }
+
+        private static async Task PopulatePositionTable(
+            SqliteConnection connection, string directoryPath, string portfolioName)
+        {
+            string scriptPath = Path.Join(directoryPath, portfolioName + ".sql");
+            using StreamReader reader = new(scriptPath);
+            await ExecuteReader(connection, reader).ConfigureAwait(false);
+        }
+
+        private static async Task ExecuteReader(SqliteConnection connection, StreamReader reader)
+        {
+            string commandText = await reader.ReadToEndAsync().ConfigureAwait(false);
+            await using SqliteCommand command = new(commandText, connection);
+            command.ExecuteReader();
         }
     }
 }
