2227486dbdcbdc3655d34d5ed6e9145a4f1b233d
Viktor T <qbit86@gmail.com>
2021-08-22 16:24:15 +0300

    Add PositionProvider

---
+++ src/Runner/PositionProvider.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace Diversifolio
+{
+    internal abstract record PositionProvider(string PortfolioName)
+    {
+        internal string PortfolioName { get; init; } =
+            PortfolioName ?? throw new ArgumentNullException(nameof(PortfolioName));
+
+        internal async Task<ImmutableDictionary<string, Position>> GetPositions()
+        {
+            ImmutableDictionary<string, Position>.Builder builder =
+                ImmutableDictionary.CreateBuilder<string, Position>(StringComparer.Ordinal);
+
+            await UncheckedPopulatePositions(builder).ConfigureAwait(false);
+            return builder.ToImmutable();
+        }
+
+        protected abstract Task UncheckedPopulatePositions(IDictionary<string, Position> positions);
+    }
+}
