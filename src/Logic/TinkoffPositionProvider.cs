fb3839313e2d8b738dc2c993bf02c47d0f62498c
Viktor T <qbit86@gmail.com>
2021-08-25 23:00:29 +0300

    Add writing the script for Position table population

--- src/Logic/TinkoffPositionProvider.cs
+++ src/Logic/TinkoffPositionProvider.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Tinkoff.Trading.OpenApi.Models;
 using Tinkoff.Trading.OpenApi.Network;
@@ -11,6 +13,8 @@ namespace Diversifolio
     public sealed record TinkoffPositionProvider(string PortfolioName, BrokerAccountType BrokerAccountType) :
         PositionProvider(PortfolioName)
     {
+        private static CultureInfo F => CultureInfo.InvariantCulture;
+
         protected override async Task PopulatePositions<TDictionary>(TDictionary positions)
         {
             if (positions is null)
@@ -28,6 +32,29 @@ namespace Diversifolio
             Portfolio portfolio = await context.PortfolioAsync(account.BrokerAccountId).ConfigureAwait(false);
             foreach (Portfolio.Position position in portfolio.Positions)
                 positions[position.Ticker] = new(position.Ticker, position.Balance);
+
+            await WriteScriptAsync(portfolio.Positions).ConfigureAwait(false);
+        }
+
+        private async Task WriteScriptAsync(List<Portfolio.Position> positions)
+        {
+            string directoryPath = Path.Join(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(Diversifolio));
+            string scriptPath = Path.Join(directoryPath, PortfolioName + ".sql");
+            await using var fileStream = new FileStream(scriptPath, FileMode.Create, FileAccess.Write, FileShare.None);
+            await using var writer = new StreamWriter(fileStream, Encoding.UTF8);
+            await writer.WriteLineAsync("INSERT INTO Position (Ticker, Balance)").ConfigureAwait(false);
+            for (int i = 0; i < positions.Count; ++i)
+            {
+                Portfolio.Position position = positions[i];
+                if (position.Ticker.Contains('\'', StringComparison.Ordinal))
+                    throw new InvalidOperationException("The ticker must not contain the apostrophe.");
+                bool isConsequent = i > 0;
+                string delimiter = isConsequent ? ",\r\n       " : "VALUES ";
+                await writer.WriteAsync(delimiter).ConfigureAwait(false);
+                string value = $"('{position.Ticker}', {Convert.ToInt32(Math.Floor(position.Balance)).ToString(F)})";
+                await writer.WriteAsync(value).ConfigureAwait(false);
+            }
         }
     }
 }
