aaa32b3927327ee5e3ce5c84e4dcf4f67cabc2ef
Viktor T <qbit86@gmail.com>
2021-08-25 23:46:50 +0300

    Add ticker "USD000UTSTOM"

--- src/Models/Boards.cs
+++ src/Models/Boards.cs
@@ -2,6 +2,7 @@ namespace Diversifolio
 {
     internal static class Boards
     {
+        internal const string Cets = "CETS";
         internal const string Tqbr = "TQBR";
         internal const string Tqob = "TQOB";
         internal const string Tqtd = "TQTD";
--- src/Models/Markets.cs
+++ src/Models/Markets.cs
@@ -2,7 +2,8 @@ namespace Diversifolio
 {
     public static class Markets
     {
-        public const string Shares = "shares";
         public const string Bonds = "bonds";
+        public const string Selt = "selt";
+        public const string Shares = "shares";
     }
 }
