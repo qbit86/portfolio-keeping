00263beba9340fb4d8d9a2f1863dc48534ab61fc
Viktor T <qbit86@gmail.com>
2021-08-27 01:13:09 +0300

    Fix Bems.CreateTuples()

--- src/Models/Moex/Bems.cs
+++ src/Models/Moex/Bems.cs
@@ -6,25 +6,25 @@ namespace Diversifolio.Moex
 {
     public static class Bems
     {
-        private static (string, string, string)[]? s_bemTuples;
+        private static (string, string, string)[]? s_tuples;
         private static Dictionary<string, Bem>? s_bemByBoard;
         private static ILookup<string, Bem>? s_bemsByEngine;
 
         public static IReadOnlyDictionary<string, Bem> BemByBoard => s_bemByBoard ??=
-            BemTuples.ToDictionary(it => it.Board, it => new Bem(it.Board, it.Engine, it.Market));
+            Tuples.ToDictionary(it => it.Board, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
 
         public static ILookup<string, Bem> BemsByEngine => s_bemsByEngine ??=
-            BemTuples.ToLookup(it => it.Engine, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
+            Tuples.ToLookup(it => it.Engine, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
 
-        private static (string Board, string Engine, string Market)[] BemTuples => s_bemTuples ??= CreateBemTuples();
+        private static (string Board, string Engine, string Market)[] Tuples => s_tuples ??= CreateTuples();
 
-        private static (string, string, string)[] CreateBemTuples() => new[]
+        private static (string, string, string)[] CreateTuples() => new[]
         {
-            (Engines.Currency, Markets.Selt, Boards.Cets),
-            (Engines.Stock, Markets.Bonds, Boards.Tqob),
-            (Engines.Stock, Markets.Shares, Boards.Tqbr),
-            (Engines.Stock, Markets.Shares, Boards.Tqtd),
-            (Engines.Stock, Markets.Shares, Boards.Tqtf)
+            (Boards.Cets, Engines.Currency, Markets.Selt),
+            (Boards.Tqob, Engines.Stock, Markets.Bonds),
+            (Boards.Tqbr, Engines.Stock, Markets.Shares),
+            (Boards.Tqtd, Engines.Stock, Markets.Shares),
+            (Boards.Tqtf, Engines.Stock, Markets.Shares)
         };
     }
 }
