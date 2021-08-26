076be268e89be996e1ddac912e6e66fd56ac7b14
Viktor T <qbit86@gmail.com>
2021-08-27 02:04:52 +0300

    Add BemsByMarket lookup

--- src/Models/Moex/Bems.cs
+++ src/Models/Moex/Bems.cs
@@ -9,12 +9,16 @@ namespace Diversifolio.Moex
         private static (string, string, string)[]? s_tuples;
         private static Dictionary<string, Bem>? s_bemByBoard;
         private static ILookup<string, Bem>? s_bemsByEngine;
+        private static ILookup<string, Bem>? s_bemsByMarket;
 
         public static IReadOnlyDictionary<string, Bem> BemByBoard => s_bemByBoard ??=
             Tuples.ToDictionary(it => it.Board, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
 
         public static ILookup<string, Bem> BemsByEngine => s_bemsByEngine ??=
             Tuples.ToLookup(it => it.Engine, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
+
+        public static ILookup<string, Bem> BemsByMarket => s_bemsByMarket ??=
+            Tuples.ToLookup(it => it.Market, it => new Bem(it.Board, it.Engine, it.Market), StringComparer.Ordinal);
 
         private static (string Board, string Engine, string Market)[] Tuples => s_tuples ??= CreateTuples();
 
