e980c78d2c3419d377026ad691075e319f5dfb4f
Viktor T <qbit86@gmail.com>
2021-08-26 23:37:53 +0300

    Add Bem record

---
+++ src/Models/Moex/Bem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Diversifolio.Moex
+{
+    public sealed record Bem(string Board, string Engine, string Market)
+    {
+        public string Board { get; } = Board ?? throw new ArgumentNullException(nameof(Board));
+        public string Engine { get; } = Engine ?? throw new ArgumentNullException(nameof(Engine));
+        public string Market { get; } = Market ?? throw new ArgumentNullException(nameof(Market));
+    }
+}
