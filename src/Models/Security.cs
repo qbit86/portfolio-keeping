0c8450d49b39beb24d09a50a22092a02b96b5e9e
Viktor T <qbit86@gmail.com>
2021-08-26 23:38:42 +0300

    Refactor accessors of Security record

--- src/Models/Security.cs
+++ src/Models/Security.cs
@@ -6,8 +6,8 @@ namespace Diversifolio
         string SecId, string BoardId, string CurrencyId, int LotSize, decimal FaceValue, int Decimals,
         decimal PrevAdmittedQuote)
     {
-        public string SecId { get; init; } = SecId ?? throw new ArgumentNullException(nameof(SecId));
-        public string BoardId { get; init; } = BoardId ?? throw new ArgumentNullException(nameof(BoardId));
-        public string CurrencyId { get; init; } = CurrencyId ?? throw new ArgumentNullException(nameof(CurrencyId));
+        public string SecId { get; } = SecId ?? throw new ArgumentNullException(nameof(SecId));
+        public string BoardId { get; } = BoardId ?? throw new ArgumentNullException(nameof(BoardId));
+        public string CurrencyId { get; } = CurrencyId ?? throw new ArgumentNullException(nameof(CurrencyId));
     }
 }
