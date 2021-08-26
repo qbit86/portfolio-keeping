0409b2de67a165cde29c058c00e8f543e9ff4683
Viktor T <qbit86@gmail.com>
2021-08-27 01:07:03 +0300

    Normalize Tickers relations

--- src/Models/Tickers.cs
+++ src/Models/Tickers.cs
@@ -7,79 +7,70 @@ namespace Diversifolio
 {
     public static class Tickers
     {
-        private static (string, AssetClass, string, string)[]? s_securities;
+        private static (string, AssetClass, string)[]? s_tuples;
         private static Dictionary<string, AssetClass>? s_assetClassByTicker;
-        private static Dictionary<string, string>? s_marketByTicker;
         private static Dictionary<string, string>? s_boardByTicker;
         private static ILookup<AssetClass, string>? s_tickersByAssetClass;
-        private static ILookup<string, string>? s_tickersByMarket;
         private static ILookup<string, string>? s_tickersByBoard;
 
-        private static (string Ticker, AssetClass AssetClass, string Market, string Board)[] Securities =>
-            s_securities ??= CreateSecurities();
-
         public static IReadOnlyDictionary<string, AssetClass> AssetClassByTicker => s_assetClassByTicker ??=
-            Securities.ToDictionary(it => it.Ticker, it => it.AssetClass, StringComparer.Ordinal);
-
-        public static IReadOnlyDictionary<string, string> MarketByTicker => s_marketByTicker ??=
-            Securities.ToDictionary(it => it.Ticker, it => it.Market, StringComparer.Ordinal);
+            Tuples.ToDictionary(it => it.Ticker, it => it.AssetClass, StringComparer.Ordinal);
 
         public static IReadOnlyDictionary<string, string> BoardByTicker => s_boardByTicker ??=
-            Securities.ToDictionary(it => it.Ticker, it => it.Board, StringComparer.Ordinal);
+            Tuples.ToDictionary(it => it.Ticker, it => it.Board, StringComparer.Ordinal);
 
         public static ILookup<AssetClass, string> TickersByAssetClass => s_tickersByAssetClass ??=
-            Securities.ToLookup(it => it.AssetClass, it => it.Ticker);
-
-        public static ILookup<string, string> TickersByMarket => s_tickersByMarket ??=
-            Securities.ToLookup(it => it.Market, it => it.Ticker, StringComparer.Ordinal);
+            Tuples.ToLookup(it => it.AssetClass, it => it.Ticker);
 
         public static ILookup<string, string> TickersByBoard => s_tickersByBoard ??=
-            Securities.ToLookup(it => it.Board, it => it.Ticker, StringComparer.Ordinal);
+            Tuples.ToLookup(it => it.Board, it => it.Ticker, StringComparer.Ordinal);
 
-        private static (string, AssetClass, string, string)[] CreateSecurities() => new[]
+        private static (string Ticker, AssetClass AssetClass, string Board)[] Tuples => s_tuples ??= CreateTuples();
+
+        private static (string, AssetClass, string)[] CreateTuples() => new[]
         {
-            ("DSKY", AssetClass.Stock, Markets.Shares, Boards.Tqbr),
-            ("FXCN", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXDE", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXDM", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXES", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXGD", AssetClass.Commodity, Markets.Shares, Boards.Tqtf),
-            ("FXIM", AssetClass.Stock, Markets.Shares, Boards.Tqtd),
-            ("FXIP", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXIT", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXMM", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXRB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXRL", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXRU", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXRW", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXTB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXTP", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("FXUS", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("FXWO", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("MGNT", AssetClass.Stock, Markets.Shares, Boards.Tqbr),
-            ("MVID", AssetClass.Stock, Markets.Shares, Boards.Tqbr),
-            ("RTKM", AssetClass.Stock, Markets.Shares, Boards.Tqbr),
-            ("SBCB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("SBGB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("SBMX", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("SBRB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("SBSP", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("SU25083RMFS5", AssetClass.Bond, Markets.Bonds, Boards.Tqob),
-            ("SU26214RMFS5", AssetClass.Bond, Markets.Bonds, Boards.Tqob),
-            ("SU29012RMFS0", AssetClass.Bond, Markets.Bonds, Boards.Tqob),
-            ("TBIO", AssetClass.Stock, Markets.Shares, Boards.Tqtd),
-            ("TECH", AssetClass.Stock, Markets.Shares, Boards.Tqtd),
-            ("TMOS", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("TSPX", AssetClass.Stock, Markets.Shares, Boards.Tqtd),
-            ("USD000UTSTOM", AssetClass.Currency, Markets.Selt, Boards.Cets),
-            ("VTBA", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("VTBB", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("VTBE", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("VTBH", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("VTBU", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("VTBX", AssetClass.Stock, Markets.Shares, Boards.Tqtf),
-            ("VTBY", AssetClass.Bond, Markets.Shares, Boards.Tqtf),
-            ("YNDX", AssetClass.Stock, Markets.Shares, Boards.Tqbr)
+            ("DSKY", AssetClass.Stock, Boards.Tqbr),
+            ("FXCN", AssetClass.Stock, Boards.Tqtf),
+            ("FXDE", AssetClass.Stock, Boards.Tqtf),
+            ("FXDM", AssetClass.Stock, Boards.Tqtf),
+            ("FXES", AssetClass.Stock, Boards.Tqtf),
+            ("FXGD", AssetClass.Commodity, Boards.Tqtf),
+            ("FXIM", AssetClass.Stock, Boards.Tqtd),
+            ("FXIP", AssetClass.Bond, Boards.Tqtf),
+            ("FXIT", AssetClass.Stock, Boards.Tqtf),
+            ("FXMM", AssetClass.Bond, Boards.Tqtf),
+            ("FXRB", AssetClass.Bond, Boards.Tqtf),
+            ("FXRL", AssetClass.Stock, Boards.Tqtf),
+            ("FXRU", AssetClass.Bond, Boards.Tqtf),
+            ("FXRW", AssetClass.Stock, Boards.Tqtf),
+            ("FXTB", AssetClass.Bond, Boards.Tqtf),
+            ("FXTP", AssetClass.Bond, Boards.Tqtf),
+            ("FXUS", AssetClass.Stock, Boards.Tqtf),
+            ("FXWO", AssetClass.Stock, Boards.Tqtf),
+            ("MGNT", AssetClass.Stock, Boards.Tqbr),
+            ("MVID", AssetClass.Stock, Boards.Tqbr),
+            ("RTKM", AssetClass.Stock, Boards.Tqbr),
+            ("SBCB", AssetClass.Bond, Boards.Tqtf),
+            ("SBGB", AssetClass.Bond, Boards.Tqtf),
+            ("SBMX", AssetClass.Stock, Boards.Tqtf),
+            ("SBRB", AssetClass.Bond, Boards.Tqtf),
+            ("SBSP", AssetClass.Stock, Boards.Tqtf),
+            ("SU25083RMFS5", AssetClass.Bond, Boards.Tqob),
+            ("SU26214RMFS5", AssetClass.Bond, Boards.Tqob),
+            ("SU29012RMFS0", AssetClass.Bond, Boards.Tqob),
+            ("TBIO", AssetClass.Stock, Boards.Tqtd),
+            ("TECH", AssetClass.Stock, Boards.Tqtd),
+            ("TMOS", AssetClass.Stock, Boards.Tqtf),
+            ("TSPX", AssetClass.Stock, Boards.Tqtd),
+            ("USD000UTSTOM", AssetClass.Currency, Boards.Cets),
+            ("VTBA", AssetClass.Stock, Boards.Tqtf),
+            ("VTBB", AssetClass.Bond, Boards.Tqtf),
+            ("VTBE", AssetClass.Stock, Boards.Tqtf),
+            ("VTBH", AssetClass.Bond, Boards.Tqtf),
+            ("VTBU", AssetClass.Bond, Boards.Tqtf),
+            ("VTBX", AssetClass.Stock, Boards.Tqtf),
+            ("VTBY", AssetClass.Bond, Boards.Tqtf),
+            ("YNDX", AssetClass.Stock, Boards.Tqbr)
         };
     }
 }
