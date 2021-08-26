0d1153ba457dbb9e97dd684f91369fd0da206240
Viktor T <qbit86@gmail.com>
2021-08-26 23:25:33 +0300

    Add class Engines

---
+++ src/Models/Moex/Engines.cs
@@ -0,0 +1,10 @@
+namespace Diversifolio.Moex
+{
+    // https://iss.moex.com/iss/engines/
+
+    public static class Engines
+    {
+        public const string Stock = "stock";
+        public const string Currency = "currency";
+    }
+}
