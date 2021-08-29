da1fe47ff8d157bd5ec97f3000dcfb113340060e
Viktor T <qbit86@gmail.com>
2021-08-29 04:11:00 +0300

    Add EnumerateUris()

--- src/Logic/DownloadHelpers.cs
+++ src/Logic/DownloadHelpers.cs
@@ -1,11 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using Diversifolio.Moex;
 
 namespace Diversifolio
 {
     internal static class DownloadHelpers
     {
-        internal static Uri BuildUri(string engine, string market, string board, string columns, string securities)
+        internal static IEnumerable<(Bem, Uri)> EnumerateUris(string market)
+        {
+            IEnumerable<Bem> bems = Bems.BemsByMarket[market];
+            foreach (Bem bem in bems)
+            {
+                Debug.Assert(market == bem.Market);
+                string columns = "";
+                string securities = "";
+                yield return (bem, BuildUri(bem.Engine, market, bem.Board, columns, securities));
+            }
+        }
+
+        private static Uri BuildUri(string engine, string market, string board, string columns, string securities)
         {
             Uri initialUri =
                 new($"https://iss.moex.com/iss/engines/{engine}/markets/{market}/boards/{board}/securities.json");
