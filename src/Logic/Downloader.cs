7857124bb6314a9452cc78b1e0238e8f08a8f097
Viktor T <qbit86@gmail.com>
2021-08-30 01:50:18 +0300

    Implement EnsureDownloaded()

--- src/Logic/Downloader.cs
+++ src/Logic/Downloader.cs
@@ -2,6 +2,7 @@ using System;
 using System.Globalization;
 using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Diversifolio
 {
@@ -20,13 +21,38 @@ namespace Diversifolio
 
         public void Dispose() => Client.Dispose();
 
-        internal static Downloader Create()
+        public static Downloader Create()
         {
             HttpClient httpClient = new();
-            string directoryName = DateTime.Now.ToString("yyyy-MM-dd_HH", F);
-            string directory = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), nameof(Diversifolio), directoryName);
+            string baseName = DateTime.Now.ToString("yyyy-MM-dd_HH", F);
+            string directory = Path.Join(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), nameof(Diversifolio), baseName);
             return new(httpClient, directory);
+        }
+
+        public async Task<string> EnsureDownloaded(string board)
+        {
+            if (board is null)
+                throw new ArgumentNullException(nameof(board));
+
+            string baseName = board + ".json";
+            string filePath = Path.Join(Directory, baseName);
+            if (File.Exists(filePath))
+                return filePath;
+
+            if (!Uris.UriByBoard.TryGetValue(board, out Uri? uri))
+            {
+                throw new ArgumentException(
+                    $"The given key '{board}' was not present in the dictionary.", nameof(board));
+            }
+
+            System.IO.Directory.CreateDirectory(Directory);
+
+            await using Stream stream = await Client.GetStreamAsync(uri).ConfigureAwait(false);
+            await using Stream destination = File.Create(filePath);
+            await stream.CopyToAsync(destination).ConfigureAwait(false);
+            await destination.DisposeAsync().ConfigureAwait(false);
+            return filePath;
         }
     }
 }
