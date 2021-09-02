063b8361b810c68c1f47fe4c2cd38152f8e8396a
Viktor T <qbit86@gmail.com>
2021-09-02 03:40:16 +0300

    Add printing tickers by market

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Diversifolio.Moex;
 
 [assembly: CLSCompliant(true)]
 
@@ -18,14 +20,22 @@ namespace Diversifolio
             PositionProvider positionProvider = PositionProviderFactory.Create(portfolioName);
             ImmutableDictionary<string, Position> positionByTicker =
                 await positionProvider.GetPositionByTickerDictionaryAsync().ConfigureAwait(false);
+
             var positions = positionByTicker.Values.OrderBy(it => it.Ticker, StringComparer.Ordinal).ToImmutableArray();
             foreach (Position position in positions)
                 await Out.WriteLineAsync(position.ToString()).ConfigureAwait(false);
 
-            using SecurityDownloader downloader = SecurityDownloader.Create();
-            // ReSharper disable once UnusedVariable
-            ImmutableDictionary<string, string> pathByBoard =
-                await downloader.GetPathByBoardDictionaryAsync().ConfigureAwait(false);
+            using var securityProvider = SecurityProvider.Create();
+            ImmutableDictionary<string, ImmutableList<Security>> securitiesByMarket =
+                await securityProvider.GetSecuritiesByMarketDictionaryAsync().ConfigureAwait(false);
+
+            await Out.WriteLineAsync().ConfigureAwait(false);
+            foreach (KeyValuePair<string, ImmutableList<Security>> kv in securitiesByMarket)
+            {
+                await Out.WriteLineAsync(kv.Key + ":").ConfigureAwait(false);
+                IEnumerable<string> tickers = kv.Value.Select(it => it.SecId);
+                await Out.WriteLineAsync("\t" + string.Join(", ", tickers)).ConfigureAwait(false);
+            }
         }
     }
 }
