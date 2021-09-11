6d3f17cccd7a99ff5bad3c8b085a7db670bc0013
Viktor T <qbit86@gmail.com>
2021-09-11 13:01:42 +0300

    Add AssetFactory.CreateBond()

--- src/Logic/AssetFactory.cs
+++ src/Logic/AssetFactory.cs
@@ -5,6 +5,26 @@ namespace Diversifolio
 {
     public static class AssetFactory
     {
+        public static Asset CreateBond(BondSecurity security, Position position)
+        {
+            if (security is null)
+                throw new ArgumentNullException(nameof(security));
+
+            if (!string.Equals(position.Ticker, security.SecId, StringComparison.Ordinal))
+            {
+                throw new ArgumentException(
+                    $"{nameof(Security.SecId)} must be equal to {nameof(Position.Ticker)}", nameof(position));
+            }
+
+            if (!Tickers.AssetClassByTicker.TryGetValue(position.Ticker, out AssetClass assetClass))
+            {
+                throw new ArgumentException(
+                    $"Could not find asset class for ticker: {position.Ticker}", nameof(position));
+            }
+
+            return UncheckedCreateBond(security, position, assetClass);
+        }
+
         public static Asset CreateShare(ShareSecurity security, Position position)
         {
             if (security is null)
@@ -13,7 +33,7 @@ namespace Diversifolio
             if (!string.Equals(position.Ticker, security.SecId, StringComparison.Ordinal))
             {
                 throw new ArgumentException(
-                    $"{nameof(security.SecId)} must be equal to {nameof(position.Ticker)}", nameof(position));
+                    $"{nameof(Security.SecId)} must be equal to {nameof(Position.Ticker)}", nameof(position));
             }
 
             if (!Tickers.AssetClassByTicker.TryGetValue(position.Ticker, out AssetClass assetClass))
@@ -23,6 +43,14 @@ namespace Diversifolio
             }
 
             return UncheckedCreateShare(security, position, assetClass);
+        }
+
+        private static Asset UncheckedCreateBond(BondSecurity security, Position position, AssetClass assetClass)
+        {
+            int balance = Convert.ToInt32(Math.Floor(position.Balance));
+            CurrencyAmount price = new(security.CurrencyId, security.PrevAdmittedQuote * security.FaceValue / 100m);
+            int decimalCount = security.Decimals - (int)Math.Log10((double)security.FaceValue) + 2;
+            return new(security.SecId, assetClass, security.BoardId, balance, price, decimalCount, security.LotSize);
         }
 
         private static Asset UncheckedCreateShare(ShareSecurity security, Position position, AssetClass assetClass)
