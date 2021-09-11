240ea04c828e51dad8c3b7d469b4536dd5b0748f
Viktor T <qbit86@gmail.com>
2021-09-11 13:16:50 +0300

    Add AssetFactory.Create()

--- src/Logic/AssetFactory.cs
+++ src/Logic/AssetFactory.cs
@@ -1,10 +1,34 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Diversifolio.Moex;
 
 namespace Diversifolio
 {
     public static class AssetFactory
     {
+        public static Asset Create(Security security, Position position)
+        {
+            if (security is null)
+                throw new ArgumentNullException(nameof(security));
+
+            if (!string.Equals(position.Ticker, security.SecId, StringComparison.Ordinal))
+            {
+                throw new ArgumentException(
+                    $"{nameof(Security.SecId)} must be equal to {nameof(Position.Ticker)}", nameof(position));
+            }
+
+            if (!Tickers.AssetClassByTicker.TryGetValue(position.Ticker, out AssetClass assetClass))
+            {
+                throw new ArgumentException(
+                    $"Could not find asset class for ticker: {position.Ticker}", nameof(position));
+            }
+
+            if (!TryUncheckedCreate(security, position, assetClass, out Asset? asset))
+                throw new ArgumentException($"Unsupported security type: {security}", nameof(security));
+
+            return asset;
+        }
+
         public static Asset CreateBond(BondSecurity security, Position position)
         {
             if (security is null)
@@ -43,6 +67,23 @@ namespace Diversifolio
             }
 
             return UncheckedCreateShare(security, position, assetClass);
+        }
+
+        private static bool TryUncheckedCreate(
+            Security security, Position position, AssetClass assetClass, [NotNullWhen(true)] out Asset? asset)
+        {
+            switch (security)
+            {
+                case ShareSecurity s:
+                    asset = UncheckedCreateShare(s, position, assetClass);
+                    return true;
+                case BondSecurity b:
+                    asset = UncheckedCreateBond(b, position, assetClass);
+                    return true;
+                default:
+                    asset = default;
+                    return false;
+            }
         }
 
         private static Asset UncheckedCreateBond(BondSecurity security, Position position, AssetClass assetClass)
