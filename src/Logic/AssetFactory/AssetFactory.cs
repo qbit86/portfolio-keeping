bb9b2722ab77b51ae7c1f15c6a382699349cec76
Viktor T <qbit86@gmail.com>
2021-09-12 12:36:35 +0300

    Add ShareAssetFactory

--- src/Logic/AssetFactory/AssetFactory.cs
+++ src/Logic/AssetFactory/AssetFactory.cs
@@ -31,33 +31,5 @@ namespace Diversifolio
 
             return asset;
         }
-
-        public static Asset CreateShare(ShareSecurity security, Position position)
-        {
-            if (security is null)
-                throw new ArgumentNullException(nameof(security));
-
-            if (!string.Equals(position.Ticker, security.SecId, StringComparison.Ordinal))
-            {
-                throw new ArgumentException(
-                    $"{nameof(Security.SecId)} must be equal to {nameof(Position.Ticker)}", nameof(position));
-            }
-
-            if (!Tickers.AssetClassByTicker.TryGetValue(position.Ticker, out AssetClass assetClass))
-            {
-                throw new ArgumentException(
-                    $"Could not find asset class for ticker: {position.Ticker}", nameof(position));
-            }
-
-            return UncheckedCreateShare(security, position, assetClass);
-        }
-
-        private static Asset UncheckedCreateShare(ShareSecurity security, Position position, AssetClass assetClass)
-        {
-            int balance = Convert.ToInt32(Math.Floor(position.Balance));
-            CurrencyAmount price = new(security.CurrencyId, security.PrevAdmittedQuote);
-            int decimalCount = security.Decimals;
-            return new(security.SecId, assetClass, security.BoardId, balance, price, decimalCount, security.LotSize);
-        }
     }
 }
---
+++ src/Logic/AssetFactory/ShareAssetFactory.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Diversifolio.Moex;
+using static Diversifolio.TryHelpers;
+
+namespace Diversifolio
+{
+    public sealed class ShareAssetFactory : AssetFactory
+    {
+        private ShareAssetFactory() { }
+
+        public static ShareAssetFactory Instance { get; } = new();
+
+        protected override bool TryUncheckedCreate(
+            Security security, Position position, AssetClass assetClass, [NotNullWhen(true)] out Asset? asset) =>
+            security is ShareSecurity shareSecurity
+                ? Success(UncheckedCreateShare(shareSecurity, position, assetClass), out asset)
+                : Failure(out asset);
+
+        private static Asset UncheckedCreateShare(ShareSecurity security, Position position, AssetClass assetClass)
+        {
+            int balance = Convert.ToInt32(Math.Floor(position.Balance));
+            CurrencyAmount price = new(security.CurrencyId, security.PrevAdmittedQuote);
+            int decimalCount = security.Decimals;
+            return new(security.SecId, assetClass, security.BoardId, balance, price, decimalCount, security.LotSize);
+        }
+    }
+}
