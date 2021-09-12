bf1876b23dff64641f04577b1ca6260f0b406133
Viktor T <qbit86@gmail.com>
2021-09-12 12:32:30 +0300

    Create BondAssetFactory

--- src/Logic/AssetFactory/AssetFactory.cs
+++ src/Logic/AssetFactory/AssetFactory.cs
@@ -32,26 +32,6 @@ namespace Diversifolio
             return asset;
         }
 
-        public static Asset CreateBond(BondSecurity security, Position position)
-        {
-            if (security is null)
-                throw new ArgumentNullException(nameof(security));
-
-            if (!string.Equals(position.Ticker, security.SecId, StringComparison.Ordinal))
-            {
-                throw new ArgumentException(
-                    $"{nameof(Security.SecId)} must be equal to {nameof(Position.Ticker)}", nameof(position));
-            }
-
-            if (!Tickers.AssetClassByTicker.TryGetValue(position.Ticker, out AssetClass assetClass))
-            {
-                throw new ArgumentException(
-                    $"Could not find asset class for ticker: {position.Ticker}", nameof(position));
-            }
-
-            return UncheckedCreateBond(security, position, assetClass);
-        }
-
         public static Asset CreateShare(ShareSecurity security, Position position)
         {
             if (security is null)
@@ -70,14 +50,6 @@ namespace Diversifolio
             }
 
             return UncheckedCreateShare(security, position, assetClass);
-        }
-
-        private static Asset UncheckedCreateBond(BondSecurity security, Position position, AssetClass assetClass)
-        {
-            int balance = Convert.ToInt32(Math.Floor(position.Balance));
-            CurrencyAmount price = new(security.CurrencyId, security.PrevAdmittedQuote * security.FaceValue / 100m);
-            int decimalCount = security.Decimals - (int)Math.Log10((double)security.FaceValue) + 2;
-            return new(security.SecId, assetClass, security.BoardId, balance, price, decimalCount, security.LotSize);
         }
 
         private static Asset UncheckedCreateShare(ShareSecurity security, Position position, AssetClass assetClass)
---
+++ src/Logic/AssetFactory/BondAssetFactory.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Diversifolio.Moex;
+using static Diversifolio.TryHelpers;
+
+namespace Diversifolio
+{
+    public sealed class BondAssetFactory : AssetFactory
+    {
+        private BondAssetFactory() { }
+
+        public static BondAssetFactory Instance { get; } = new();
+
+        protected override bool TryUncheckedCreate(
+            Security security, Position position, AssetClass assetClass, [NotNullWhen(true)] out Asset? asset) =>
+            security is BondSecurity bondSecurity
+                ? Success(UncheckedCreateBond(bondSecurity, position, assetClass), out asset)
+                : Failure(out asset);
+
+        private static Asset UncheckedCreateBond(BondSecurity security, Position position, AssetClass assetClass)
+        {
+            int balance = Convert.ToInt32(Math.Floor(position.Balance));
+            CurrencyAmount price = new(security.CurrencyId, security.PrevAdmittedQuote * security.FaceValue / 100m);
+            int decimalCount = security.Decimals - (int)Math.Log10((double)security.FaceValue) + 2;
+            return new(security.SecId, assetClass, security.BoardId, balance, price, decimalCount, security.LotSize);
+        }
+    }
+}
