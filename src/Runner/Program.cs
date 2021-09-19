0c7e6c199d05152ee63263777b684dd9dc1c9f1b
Viktor T <qbit86@gmail.com>
2021-09-20 01:04:26 +0300

    Add printing proportions

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace Diversifolio
 {
     internal static class Program
     {
+        private static readonly AssetClass[] s_assetClasses = { AssetClass.Stock, AssetClass.Other };
+
+        private static CultureInfo P => CultureInfo.InvariantCulture;
         private static TextWriter Out => Console.Out;
 
         private static async Task Main()
@@ -29,12 +33,13 @@ namespace Diversifolio
             AssetProvider<RubCurrencyConverter> assetProvider = new(securitiesByMarket, currencyConverter);
             IReadOnlyList<Asset> assets = assetProvider.GetAssets(positions);
 
-            ILookup<bool, Asset> assetsByClass = assets.ToLookup(it => it.AssetClass == AssetClass.Stock);
-            for (int i = 0; i < 2; ++i)
+            ILookup<AssetClass, Asset> assetsByClass = assets.ToLookup(GetAssetClass);
+
+            for (int i = 0; i < s_assetClasses.Length; ++i)
             {
                 if (i > 0)
                     await Out.WriteLineAsync("-----------------------------------------").ConfigureAwait(false);
-                bool key = !Convert.ToBoolean(i);
+                AssetClass key = s_assetClasses[i];
                 IEnumerable<Asset> grouping = assetsByClass[key];
                 IOrderedEnumerable<Asset> orderedAssets = grouping
                     .OrderBy(it => it.OriginalPrice.Currency)
@@ -42,6 +47,25 @@ namespace Diversifolio
                 foreach (Asset asset in orderedAssets)
                     await Out.WriteLineAsync(AssetFormatter.Shared.Format(asset)).ConfigureAwait(false);
             }
+
+            decimal totalValue = assets.Sum(it => it.Value.Amount);
+            await Out.WriteLineAsync().ConfigureAwait(false);
+            foreach (AssetClass key in s_assetClasses)
+            {
+                decimal sum = assetsByClass[key].Sum(it => it.Value.Amount);
+                decimal ratio = sum / totalValue;
+                await Out.WriteLineAsync($"{key}\t| {sum.ToString("F2", P),10} | {ratio.ToString("P2", P),8}")
+                    .ConfigureAwait(false);
+            }
+
+            await Out.WriteLineAsync($"Total\t| {totalValue.ToString("F2", P),10}")
+                .ConfigureAwait(false);
         }
+
+        private static AssetClass GetAssetClass(Asset asset) => asset.AssetClass switch
+        {
+            AssetClass.Stock => AssetClass.Stock,
+            _ => AssetClass.Other
+        };
     }
 }
