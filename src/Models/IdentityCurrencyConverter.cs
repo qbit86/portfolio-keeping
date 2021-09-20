c7692346567b4d8f131b81b11cea86db092dacab
Viktor T <qbit86@gmail.com>
2021-09-20 03:02:06 +0300

    Add IdentityCurrencyConverter

---
+++ src/Models/IdentityCurrencyConverter.cs
@@ -0,0 +1,13 @@
+namespace Diversifolio
+{
+    public sealed class IdentityCurrencyConverter : ICurrencyConverter
+    {
+        public static IdentityCurrencyConverter Instance { get; } = new();
+
+        public bool TryConvertFrom(CurrencyAmount source, out CurrencyAmount result)
+        {
+            result = source;
+            return true;
+        }
+    }
+}
