e2236dfdcd098e512b971ddeeb15a7f052f2c9fb
Viktor T <qbit86@gmail.com>
2021-09-20 03:21:39 +0300

    Add TODO

--- src/Logic/MulticurrencyAmount.cs
+++ src/Logic/MulticurrencyAmount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Diversifolio
 {
+    // TODO: Make readonly struct.
     public sealed class MulticurrencyAmount
     {
         private readonly Dictionary<string, CurrencyAmount> _currencyAmountByCurrency = new(StringComparer.Ordinal);
