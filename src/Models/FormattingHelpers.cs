d6ba07647672ac2990ad7663cfd8d3bb26b6f517
Viktor T <qbit86@gmail.com>
2021-10-01 00:58:36 +0300

    Add FormattingHelpers.AppendAligned()

--- src/Models/FormattingHelpers.cs
+++ src/Models/FormattingHelpers.cs
@@ -13,6 +13,9 @@ namespace Diversifolio
         internal static void AppendDecimal(
             StringBuilder stringBuilder, decimal value, string? format, IFormatProvider? formatProvider)
         {
+            if (stringBuilder is null)
+                throw new ArgumentNullException(nameof(stringBuilder));
+
             // src/libraries/System.Private.CoreLib/src/System/Number.NumberBuffer.cs
             const int decimalNumberBufferLength = 29 + 1 + 1;
             Span<char> buffer = stackalloc char[decimalNumberBufferLength];
@@ -20,6 +23,64 @@ namespace Diversifolio
                 stringBuilder.Append(buffer[..charsWritten]);
             else
                 stringBuilder.Append(value.ToString(format, formatProvider));
+        }
+
+        internal static int AppendAligned(
+            StringBuilder stringBuilder, ReadOnlySpan<char> value, int desiredWidth, bool padLeft = false)
+        {
+            if (stringBuilder is null)
+                throw new ArgumentNullException(nameof(stringBuilder));
+
+            return UncheckedAppendAligned(stringBuilder, value, desiredWidth, padLeft);
+        }
+
+        internal static int AppendLeft(StringBuilder stringBuilder, ReadOnlySpan<char> left, int desiredWidth)
+        {
+            if (stringBuilder is null)
+                throw new ArgumentNullException(nameof(stringBuilder));
+
+            return UncheckedAppendAligned(stringBuilder, left, desiredWidth, false);
+        }
+
+        internal static int AppendRight(StringBuilder stringBuilder, ReadOnlySpan<char> right, int desiredWidth)
+        {
+            if (stringBuilder is null)
+                throw new ArgumentNullException(nameof(stringBuilder));
+
+            return UncheckedAppendAligned(stringBuilder, right, desiredWidth, true);
+        }
+
+        private static int UncheckedAppendAligned(
+            StringBuilder stringBuilder, ReadOnlySpan<char> value, int desiredWidth, bool padLeft)
+        {
+            int initialLength = stringBuilder.Length;
+            int paddingWidth = desiredWidth - value.Length;
+            if (paddingWidth <= 0)
+            {
+                stringBuilder.Append(value);
+                return ActualWidth();
+            }
+
+            Span<char> padding = stackalloc char[paddingWidth];
+            padding.Fill(' ');
+
+            if (padLeft)
+            {
+                stringBuilder.Append(padding);
+                stringBuilder.Append(value);
+            }
+            else
+            {
+                stringBuilder.Append(value);
+                stringBuilder.Append(padding);
+            }
+
+            return ActualWidth();
+
+            int ActualWidth()
+            {
+                return stringBuilder.Length - initialLength;
+            }
         }
 
         private static CultureInfo CreateFormatProvider()
