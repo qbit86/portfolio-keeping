d980d31d0f962ba973d159e2634a65d68cd8c697
Viktor T <qbit86@gmail.com>
2022-01-23 13:11:16 +0300

    Add `app.UseDeveloperExceptionPage()`

--- src/RazorRunner/Program.cs
+++ src/RazorRunner/Program.cs
@@ -10,7 +10,11 @@ builder.Services.AddRazorPages();
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
