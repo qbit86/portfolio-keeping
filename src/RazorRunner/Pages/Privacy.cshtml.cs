fba3c9abad62dd0e18d572d7eefcdb7fc22a3105
Viktor T <qbit86@gmail.com>
2022-01-23 13:01:15 +0300

    Cleanup C# code

--- src/RazorRunner/Pages/Privacy.cshtml.cs
+++ src/RazorRunner/Pages/Privacy.cshtml.cs
@@ -7,10 +7,7 @@ public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
+    public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
 
     public void OnGet() { }
 }
--- src/RazorRunner/Program.cs
+++ src/RazorRunner/Program.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
