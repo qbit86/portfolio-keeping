545d3bc335d80415e85d3f1c164b0701aabd4cab
Viktor T <qbit86@gmail.com>
2022-01-23 13:07:36 +0300

    Fix namespaces

--- src/RazorRunner/Pages/Error.cshtml.cs
+++ src/RazorRunner/Pages/Error.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace RazorRunner.Pages;
+namespace Diversifolio.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
--- src/RazorRunner/Pages/Index.cshtml.cs
+++ src/RazorRunner/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace RazorRunner.Pages;
+namespace Diversifolio.Pages;
 
 public class IndexModel : PageModel
 {
