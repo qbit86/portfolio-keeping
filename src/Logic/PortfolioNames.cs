a72859d0c6fe55af9fe014f6181b85d277d6585e
Viktor T <qbit86@gmail.com>
2022-02-05 13:58:52 +0300

    Add constant for Test portfolio name

--- src/Logic/PortfolioNames.cs
+++ src/Logic/PortfolioNames.cs
@@ -3,6 +3,7 @@ namespace Diversifolio;
 public static class PortfolioNames
 {
     public const string Sber = nameof(Sber);
+    public const string Test = nameof(Test);
     public const string Tinkoff = nameof(Tinkoff);
     public const string TinkoffIis = nameof(TinkoffIis);
     public const string Vtb = nameof(Vtb);
